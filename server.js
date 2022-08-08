8b183ab83c3f5263cd81afce16489ceaab2c3beb
palakollu145 <83295582+palakollu145@users.noreply.github.com>
2022-08-08 13:04:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h2 style="color: purple">Successfully running the Node App!<h2>');
+  res.send("<body style='background-color:blue;' ><h1 style='color:white;'>Hello Before</h1></body>");
 });
 
 app.listen(PORT);
